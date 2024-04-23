1bb3d3d7fd782c66562e9d28f14729f5291e0dda
mmilojevic-dev <mmilojevic.dev@gmail.com>
2024-04-24 01:21:45 +0200

    Implementing persistent filtering via browser url query params

--- src/features/filters/hooks/useFilters.ts
+++ src/features/filters/hooks/useFilters.ts
@@ -1,22 +1,61 @@
 import { zodResolver } from '@hookform/resolvers/zod'
+import { format, parseISO } from 'date-fns'
+import { useEffect, useMemo } from 'react'
 import { useForm, useWatch } from 'react-hook-form'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 import { useDebounce } from '@/hooks'
 
 import { filtersConfig } from '../config'
 import { FiltersFormSchemaType } from '../types'
 
-export type UseFiltersReturnType = ReturnType<typeof useFilters>
-
 export const useFilters = (debounceDelay = 500) => {
+  const navigate = useNavigate()
+  const location = useLocation()
+
+  const params = useMemo(
+    () => new URLSearchParams(location.search),
+    [location.search]
+  )
+
+  const defaultValues = useMemo(() => {
+    return Object.keys(filtersConfig.default).reduce(
+      (acc, key) => {
+        const paramValue = params.get(key)
+        if (paramValue) {
+          acc[key] =
+            key === 'fromDate' || key === 'toDate'
+              ? parseISO(paramValue)
+              : paramValue
+        }
+        return acc
+      },
+      { ...filtersConfig.default }
+    )
+  }, [params])
+
   const form = useForm<FiltersFormSchemaType>({
     resolver: zodResolver(filtersConfig.schema),
-    defaultValues: filtersConfig.default,
+    defaultValues,
     mode: 'onChange'
   })
 
-  const values = useWatch({ control: form.control })
-  const debouncedValues = useDebounce(values, debounceDelay)
+  const filters = useWatch({ control: form.control })
+  const debouncedFilters = useDebounce(filters, debounceDelay)
 
-  return { form, filters: debouncedValues }
+  useEffect(() => {
+    const queryString = Object.entries(debouncedFilters)
+      .filter(([, value]) => value)
+      .map(([key, value]) => {
+        if (!value) return
+        const formattedValue =
+          value instanceof Date ? format(value, 'yyyy-MM-dd') : value.toString()
+        return `${encodeURIComponent(key)}=${encodeURIComponent(formattedValue)}`
+      })
+      .join('&')
+
+    navigate(`?${queryString}`, { replace: true })
+  }, [debouncedFilters, navigate])
+
+  return { form, filters: debouncedFilters }
 }
--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export const useDebounce = <T>(value: T, delay: number = 500): T => {
+export const useDebounce = <T>(value: T, delay: number = 300): T => {
   const [debouncedValue, setDebouncedValue] = React.useState<T>(value)
 
   React.useEffect(() => {
